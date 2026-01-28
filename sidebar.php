15d0034d5e2918bd0f596a143a12d52b45dcd9d6
Evan0404 <evanlum4jang@gmail.com>
2026-01-28 11:46:00 +0700

    feat: Update sidebar logo link to use app name instead of image

--- sidebar.php
+++ sidebar.php
@@ -6,7 +6,9 @@ $getHal = sani($_GET['hal'] ?? 'dashboard');
         <div class="sidebar-header">
             <div class="d-flex justify-content-between">
                 <div class="logo">
-                    <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo" srcset=""></a>
+                    <a href="index.php">
+                        <h3><?php echo $appName; ?></h3>
+                    </a>
                 </div>
                 <div class="toggler">
                     <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
